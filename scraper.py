24fccd8d3b429f48da027b7c605eb0f1101c1cab
gogodjames <jamesgogod@gmail.com>
2025-01-09 11:46:25 -0800

    Added concurrency and seperated get_reqs into two sub functions: process_row and get_prereqs, these changes have turned the run time from 1:30 seconds to about 3 seconds.

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
-import html
+from concurrent.futures import ThreadPoolExecutor
 
 # Seed URL: https://catalogue.uci.edu/undergraduatedegrees/
 SEED_URL = "https://catalogue.uci.edu/undergraduatedegrees/"
@@ -50,32 +50,45 @@ def get_reqs(url):
 
     course_table = soup.find('table', class_="sc_courselist")
     rows = course_table.find_all('tr')
-    for row in rows:
-        code_cell = row.find('td', class_="codecol")
-        if code_cell:
-            code = code_cell.get_text(strip=True)
-            
-            a_tag = code_cell.find('a')
-            desc_href = a_tag.get('href')
-            desc_url_info = requests.get("https://catalogue.uci.edu" + desc_href, headers=HEADER)
-            desc_url_info.raise_for_status()
-            soup = BeautifulSoup(desc_url_info.content, 'html.parser')
-            inside_div = soup.find('div', {'id': 'fssearchresults', 'class':'searchresults'})
-            prereqs = []
-            links = inside_div.find_all('p')
-            for link in links:
-                text = link.get_text(strip=True)
-                if 'prerequisite' in text.lower():
-                    atags = link.find_all('a')
-                    for atag in atags:
-                        prereqs.append(atag.get_text().replace('\xa0', " "))
-            reqs.append({'code': code, 'description': prereqs})
-    return reqs
 
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        reqs = list(executor.map(process_row, rows))
+    return reqs
     
+def process_row(row):
+    code_cell = row.find('td', class_="codecol")
+    if code_cell:
+        code = code_cell.get_text(strip=True)
+        a_tag = code_cell.find('a')
+        if a_tag:
+            desc_href = a_tag.get('href')
+            prereqs = get_prereqs(desc_href)
+            return({'code': code, 'description': prereqs})
+
+def get_prereqs(desc_href):
+    desc_url_info = requests.get("https://catalogue.uci.edu" + desc_href, headers=HEADER)
+    desc_url_info.raise_for_status()
+    soup = BeautifulSoup(desc_url_info.content, 'html.parser')
+    inside_div = soup.find('div', {'id': 'fssearchresults', 'class':'searchresults'})
+    prereqs = []
+    links = inside_div.find_all('p')
+    for link in links:
+        text = link.get_text(strip=True)
+        if 'prerequisite' in text.lower():
+            atags = link.find_all('a')
+            for atag in atags:
+                prereqs.append(atag.get_text().replace('\xa0', " "))
+    return prereqs
+
 if __name__ == '__main__':
     #print(extract_majors_and_minors())
     example_url = "https://catalogue.uci.edu/schoolofphysicalsciences/departmentofphysicsandastronomy/physics_bs/#requirementstext"
     courses = get_reqs(example_url)
     for course in courses:
+        if course is None:
+            continue
+        if course['code'] is None:
+            course['code'] = "None"
+        if course['description'] is None:
+            course['description'] = "None"
         print(f"Code: {course['code']}, Description: {course['description']}")
