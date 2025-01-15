1bc1c933fe797d07f0c35da6d901e7d88acd1901
Evan Velek <evelek321@gmail.com>
2025-01-14 22:28:32 -0800

    Adds ap credit dictionary

---
+++ ap_credit.py
@@ -0,0 +1,69 @@
+
+
+AP_CLASSES = {
+    "African American Studies (3, 4, 5)": (4, "A"),
+    "Art History (3)": (8, "ART HIST 40A, VIII"),
+    "Art History (4, 5)": (8, "ART HIST 40A, ART HIST 40B, VIII"),
+    "Drawing (3, 4, 5)": (8, ""),
+    "2-D Art and Design (3, 4, 5)": (8, ""),
+    "3-D Art and Design (3, 4, 5)": (8, ""),
+    "Biology (3, 4, 5)": (8, "II"), # No II for bio sci majors
+    "Capstone Reseaerch and Seminar": (0, "G"),
+    "Chemistry (3)": (8, ""),
+    "Chemistry (4, 5)": (8, "CHEM 1A || ENGR 1A"),
+    "Chinese Language and Culture (3)": (8, ""),
+    "Chinese Language and Culture (4, 5)": (8, "Chinese 1A, Chinese 1B, Chinese 1C, Chinese 2A, VI, VIII)") ,
+    "Computer Science A (3, 4, 5)": (8, ""),
+    "Computer Science Principles (3, 4, 5)": (8, "I&C SCI 20"),
+    "Macroeconomics (3)": (4, ""),
+    "Macroeconomics (4, 5)": (4, "ECON 20B || MGMT 4B"),
+    "Microeconomics (3)": (4, ""),
+    "Microeconomics (4, 5)": (4, "ECON 20A || MGMT 4A"),
+    "Language and Composition (3)": (8, "Entry Writing"),
+    "Language and Composition (4, 5)": (8, "WRITING 50, IV"), # Not for Literary Journalism, Enlighs, or school of humanities requirements
+    "Literature and Composition (3)": (8, "Entry Writing"),
+    "Literature and Composition (4, 5)": (8, "WRITING 50, IV"), # Not for Literary Journalism, Enlighs, or school of humanities requirements
+    "Environmental Science (3)": (4, 0),
+    "Environmental Science (4, 5)": (4, "EARTHSS 1"),
+    "French Language and Culture (3)": (8, "FRENCH 1A, FRENCH 1B, FRENCH 1C, VI"),
+    "French Language and Culture (4, 5)": (8, "FRENCH 2A, FRENCH 2B, FRENCH 2C, VI, VIII"),
+    "Human Geography (3, 4, 5)": (4, ""),
+    "German Language and Culture (3)": (8, "GERMAN 1A, GERMAN 1B, GERMAN 1C, VI"),
+    "German Language and Culture (4, 5)": (8, "GERMAN 2A, GERMAN 2B, GERMAN 2C, VI, VIII"),
+    "U.S. Government and Politics (3)": (4, ""),
+    "U.S. Government and Politics (4, 5)": (4, "POL SCI 21A, American Institutions"), # Not for School of Social Sciences except political science
+    "Comparative Government and Politics (3)": (4, ""),
+    "Comparative Government and Politics (4, 5)": (4, "POL SCI 51A"),# Not for social sciences majors expect political science
+    "European History (3, 4)": (8, ""),
+    "European History (5)": (8, "HISTORY 40A || HISTORY 40B || HISTORY 40C, IV, American Institutions"), # Maybe not repalce school of humanities requirements
+    "United States History (3, 4)": (8, "American Institutions"),
+    "United States History (5)": (8, "HISTORY 40A || HISTORY 40B || HISTORY 40C, IV, American Institutions"), # May not replace school of humanities requirements
+    "Modern World History (3, 4)": (8, ""),
+    "Modern World History (5)": (8, "HISTORY 21B || HISTORY 21C, IV, VIII"), # may not replace school of humantiies requirements
+    "Italian Language and Culture (3)": (8, "ITALIAN 1A, ITALIAN 1B, ITALIAN 1C, VI"),
+    "Italian Language and Culture (4, 5)": (8, "ITALIAN 2A, ITALIAN 2B, ITALIAN 2C, VI, VIII"),
+    "Japanese Language and Culture (3)": (8, "JAPANESE 1A, JAPANESE 1B, JAPANESE 1C, VI"),
+    "Japanese Language and Culture (4, 5)": (8, "JAPANESE 2A, JAPANESE 2B, JAPANESE 2C, VI, VIII"),
+    "Latin (3)": (8, ""),
+    "Latin (4, 5)": (8, "VI, VIII"), # Towards Classics major or School of Humanities language requirement
+    "Calculus AB (3, 4, 5)": (4, "MATH 2A || MATH 5A"),
+    "Calculus BC (3)": (8, "MATH 2A || MATH 5A"),
+    "Calculus BC (4, 5)": (8, "MATH 2A || MATH 5A, MATH 2B || MATH 5B"),
+    "Precalculus (4, 5)": (0, "G"),
+    "Music Theory (3, 4, 5)": (8, ""),
+    "Physics 1 (3, 4, 5)": (8, ""),
+    "Physics 2 (3, 4, 5)": (8, ""),
+    "Physics C (Part I or II) (3)": (4, ""),
+    "Physics C (Part I or II) (4)": (4, "PHYSICS 2"),
+    "Physics C Part I (5)": (4, "PHYSICS 3A"),
+    "Physics C Part II (5)": (4, "PHYSICS 3C"),
+    "Psychology (3)": (4, ""),
+    "Psychology (4, 5)": (4, "PSCI 9 || PSYCH 7A"),
+    "Research (3, 4, 5)": (0, "Entry Writing"),
+    "Seminar (3, 4, 5)": (0, "Entry Writing"),
+    "Spanish Language and Culture (3)": (8, "SPANISH 1A, SPANISH 1B, SPANISH 1C, VI"),
+    "Spanish Language and Culture (4, 5)": (8, "SPANISH 2A, SPANISH 2B, SPANISH 2C, VI, VIII"),
+    "Spanish Literature and Culture (3)": (8, "SPANISH 1A, SPANISH 1B, SPANISH 1C, VI"),
+    "Spanish Literature and Culture (4, 5)": (8, "SPANISH 3, SPANISH 3H, VI, VII, VIII"),
+    "Statistics (3, 4, 5)": (4, "STATS 7 || STARTS 8 || MGMT 7 || SOCECOL 13 || EDUC 15")
+}
